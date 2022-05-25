3297b28967599c85d9347e9657d1d52eadfdd2f5
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 15:23:24 -0500

    initial python

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,17 @@
-def credit_check(str):
-    pass
-
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
-
+def credit_check(cardnum):
+    from functools import reduce
+    arr = []
+    for i in range(0, len(cardnum)):
+        if (i % 2 == 0):
+            if (int(cardnum[i]) * 2 < 10):
+                arr.append(int(cardnum[i]) * 2)
+            else:
+                arr.append((int(cardnum[i]) * 2) - 9)
+        else:
+            arr.append(int(cardnum[i]))
+    if (reduce(lambda a, b: a + b, arr) % 10 == 0):
+        return ('The number is valid!')
+    else:
+        return ('The number is invalid!')
+#print(credit_check('5541808923795240')) #valid
+#print(credit_check("4024007106512380")) #invalid
